a463222e69c7e1e78262fe936ae0160447c654b9
serkan <s.safran84@gmail.com>
2021-04-13 10:32:24 +0200

    exercise finished

---
+++ Data/UserContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Data
+{
+    public class UserContext: DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
